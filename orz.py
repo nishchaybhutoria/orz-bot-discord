38c3b48398953b7fd2e9bece54566d7040e0111d
Nishchay Bhutoria <43699361+nishchaybhutoria@users.noreply.github.com>
2020-11-04 10:58:51 +0530

    Update orz.py
    Added new commands!

--- orz.py
+++ orz.py
@@ -27,11 +27,11 @@ async def on_message(message):
     if message.content == 'plz job':
         response = 'bro i want job at Goggle brooo plzz'
         await message.channel.send(response)
-    if message.content == 'plz help':
+    if message.content == 'plz halp':
         response = 'broo help in this problemm plzz..this is not from ongoing contest brooooo :pray::pray::pray:'
         await message.channel.send(response)
     if message.content == 'plz help bot':
-        response = "```yaml\nCommands with mandatory prefix plz:\norz\ngreet\nroadmap\ntestcase\njob\nhelp\ntrivial\nrequest\ncolin\nhw\nvises\nnishuz```"
+        response = "```yaml\nCommands with mandatory prefix plz:\norz\ngreet\nroadmap\ntestcase\njob\nhelp\ntrivial\nrequest\ncolin\nhw\nvises\nnishuz\nbro\nkms\nred```"
         await message.channel.send(response)
     if message.content == 'plz trivial':
         response = "plzz do proper bro so trivial broo"
@@ -54,7 +54,28 @@ async def on_message(message):
     if message.content == 'plz vises':
         response = "plz give me smart like vishesh broooo :pray:"
         await message.channel.send(response)
+    if message.content == 'plz red':
+        response = "hii brooo am grey now plz halp.. how to red in 3 months????"
+        await message.channel.send(response)
+    if message.content == 'plz bro':
+        response = "plz brooooo plzzzz plzzz plzzz???? <:ughh:751118102254583848>"
+        await message.channel.send(response)
+    if message.content == 'plz uBun2':
+        response = "plz brooooo plzzzz plzzz plzzz your cf ID???? <:ughh:751118102254583848>"
+        await message.channel.send(response)
+    if message.content == 'plz antiorz':
+        response = "bro plzzz no orz me bro i retard"
+        await message.channel.send(response)
+    if message.content == 'plz kms':
+        response = "galen colinnnnn broooo"
+        await message.channel.send(response)
+    #if message.content == 'plz IGM':
+    #    for orz in range(100):
+    #        response = "<@!145266128363585536> IGM <:liorz:751117138793726082> <:liorz:751117138793726082> <:liorz:751117138793726082> <:liorz:751117138793726082> <:liorz:751117138793726082> woohooooo"
+    #        await message.channel.send(response)
 @client.event
 async def on_ready():
      print("Connected!")
 client.run(TOKEN)
+
+
